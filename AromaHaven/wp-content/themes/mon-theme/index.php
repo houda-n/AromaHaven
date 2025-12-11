4e453deca97be09c985a1673515ef730931f884c
Nourelhouda Elbaz <nelbaz@grouperf.com>
2025-12-11 09:40:00 +0100

    fix pages

--- AromaHaven/wp-content/themes/mon-theme/index.php
+++ AromaHaven/wp-content/themes/mon-theme/index.php
@@ -1,12 +1,8 @@
-<?php
-get_header();
-?>
-<main>
-  <h1><?php bloginfo('name'); ?></h1>
-  <p><?php bloginfo('description'); ?></p>
-</main>
-<?php
-bloginfo();
-the_title();
-the_content();
-get_footer();
+<?php get_header(); ?>
+
+<div class="container">
+    <h1><?php bloginfo('name'); ?></h1>
+    <p><?php bloginfo('description'); ?></p>
+</div>
+
+<?php get_footer(); ?>
